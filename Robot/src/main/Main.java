b47498df922cad0f1d562cddf9e9f392443cfad7
ScottSewell <scott.sewell@mail.mcgill.ca>
2016-11-08 15:16:36 -0500

    Robot can actually move!

--- Robot/src/main/Main.java
+++ Robot/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 import lejos.hardware.Button;
 
 /**
@@ -13,6 +15,7 @@ public class Main
     private Board m_board;
     private Odometer m_odometer;
     private Driver m_driver;
+    private Display m_display;
     
     /**
      * Launches the main program.
@@ -28,9 +31,16 @@ public class Main
      */
     private void launch()
     {
+        // initilize
+        m_odometer = new Odometer();
+        m_display = new Display(m_odometer);
+        m_driver = new Driver(m_odometer);
+        
         // start initialization when ready.
+        m_display.printString("Press a button!");
         Button.waitForAnyPress();
         
+        /*
         // wait to progress until start information is received via wifi
         m_startParams = new StartParameters();
         while (!m_startParams.hasRecievedData())
@@ -38,14 +48,28 @@ public class Main
             m_startParams.getWifiData();
             //m_startParams.useTestData(); // use for testing to avoid having to launch wifi server
         }
-        
         // get the board
         m_board = m_startParams.getBoard();
+        */
+
+        // start threads
+        m_odometer.start();
+        m_display.start();
         
-        // activate the odometer
-        Odometer odometer = new Odometer();
-        odometer.start();
+        List<Vector2> waypoints = new ArrayList<Vector2>();
+        waypoints.add(new Vector2(60, 0));
+        waypoints.add(new Vector2(60, -60));
+        waypoints.add(new Vector2(30, -60));
+        waypoints.add(new Vector2(0, 0));
         
-        m_driver = new Driver(odometer);
+        while (waypoints.size() > 0)
+        {
+            m_driver.travelTo(waypoints.get(0));
+            while (m_driver.isTravelling() && !m_driver.isNearDestination()) { }
+            waypoints.remove(0);
+        }
+        
+        // finish
+        System.exit(0);
     }
 }
--- Robot/src/main/Robot.java
+++ Robot/src/main/Robot.java
@@ -26,15 +26,15 @@ public class Robot
     public static final TextLCD                 SCREEN              = LocalEV3.get().getTextLCD();
     
     // robot dimensions
-    public static final float   RADIUS          = 18.0f;                // furthest point on robot from the center of rotation in cm
+    public static final float   RADIUS          = 20.0f;                // furthest point on robot from the center of rotation in cm
     public static final float   WHEEL_RADIUS    = 2.13f;                // radius of wheels in cm
-    public static final float   WHEEL_TRACK     = 8.25f;                // distance between wheels in cm
+    public static final float   WHEEL_TRACK     = 16.0f;                // distance between wheels in cm
     public static final Vector2 US_OFFSET       = new Vector2(0,0);     // the position offset of the ultrasound sensor from the robot center in cm
     public static final Vector2 CSL_OFFSET      = new Vector2(0,0);     // the position offset of the left color sensor from the robot center in cm
     public static final Vector2 CSR_OFFSET      = new Vector2(0,0);     // the position offset of the right color sensor from the robot center in cm
     
     // dynamic properties 
-    public static final int     ACCELERATION    = 800;      // acceleration of the wheel motors
+    public static final int     ACCELERATION    = 1200;      // acceleration of the wheel motors
     public static final int     MOVE_SPEED      = 150;      // maximum wheel rotation speed while moving forwards in deg/sec
     public static final int     ROTATE_SPEED    = 100;      // maximum wheel rotation speed while turning in deg/sec
 }
